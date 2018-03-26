3d8d804548644b6edc1eb2d3febc04bb222a6b23
AppaBabba <darleg@outlook.com>
2018-03-25 17:38:28 -0700

    add commits

--- WLScode/oop/oopv1.cpp
+++ WLScode/oop/oopv1.cpp
@@ -1,15 +1,18 @@
 #include<iostream>
 using namespace std;
+// ok, a very simple class
 class pram
 {
         private:
-        int a,c;
+        int a,c; // two private data members
         public:
+// A constructor with default values
         pram()
         {
-            a=0;
-            c=0;
+            a=1;
+            c=31;
         }
+// A function to input change to data members
         void input()
         {
              cout<<"input attr: ";
@@ -17,6 +20,7 @@ class pram
              cout<<"input color: ";
              cin>>c;
          }
+// A function to output the object data
          void output()
          {
              cout<<"attr is: "<<a<<endl;
