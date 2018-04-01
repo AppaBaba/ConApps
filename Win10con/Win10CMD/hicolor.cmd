5c84550c1497c31c48f271c895a3fe8c41c8eaa6
AppaBabba <darleg@outlook.com>
2018-04-01 09:21:05 -0700

    remove and replace directories

---
+++ Win10con/Win10CMD/hicolor.cmd
@@ -0,0 +1,15 @@
+@echo off
+cls
+echo  [31mHi there[0m
+echo  [32mHi there[0m
+echo  [33mHi there[0m
+echo  [34mHi there[0m
+echo  [35mHi there[0m
+echo  [36mHi there[0m
+echo  [37mHi there[0m
+echo  [41mHi there[0m
+echo  [42mHi there[0m
+echo  [43mHi there[0m
+echo  [44mHi there[0m
+echo  [45mHi there[0m
+echo  [46mHi there[0m
