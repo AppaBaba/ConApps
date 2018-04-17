c3a7dc25edff0becf2224213f73842ef28f42e31
AppaBabba <darleg@outlook.com>
2018-04-16 22:09:05 -0700

    testnand

--- WLScode/AI/Prolog/gates.pl
+++ WLScode/AI/Prolog/gates.pl
@@ -4,4 +4,20 @@ and(0,0,0).
 and(0,1,0).
 and(1,0,0).
 and(1,1,1).
-nand(In1,In2,Out):-and(In1,In2,Node), inv(Node,Out).
+
+nand(In1,In2,Out):-and(In1,In2,Node),
+			inv(Node,Out).
+
+xor(NodeX,NodeY,NodeZ):-and(NodeX,NodeY,NodeA),
+			and(NodeX,NodeA,NodeB),
+			and(NodeY,NodeA,NodeC),
+			and(NodeB,NodeC,NodeZ).
+
+testnand:-nand(0,0,Out),
+	write(Out),nl,
+	nand(1,0,Out),
+	write(Out),nl,
+	nand(0,1,Out),
+	write(Out),nl,
+	nand(1,1,Out),
+	write(Out),nl.
