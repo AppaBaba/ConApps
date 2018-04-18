bfa9b6f9f29af2663e018c3c50c0a53ca38dadd8
AppaBabba <darleg@outlook.com>
2018-04-17 21:44:03 -0700

    fulladder put in

--- WLScode/AI/Prolog/gates.pl
+++ WLScode/AI/Prolog/gates.pl
@@ -4,6 +4,10 @@ and(0,0,0).
 and(0,1,0).
 and(1,0,0).
 and(1,1,1).
+or(0,0,0).
+or(1,0,1).
+or(0,1,1).
+or(1,1,1).
 
 nand(A,B,NZ):-and(A,B,C),
 		inv(C,NZ).
@@ -15,3 +19,9 @@ xor(A,B,S):-nand(A,B,D),
 
 halfadder(A,B,Sum,Carry):-xor(A,B,Sum),
 			and(A,B,Carry).
+
+fulladder(A,B,Cin,Sum,Carry):-xor(A,B,S1),
+			and(A,B,C1),
+			xor(S1,Cin,Sum),
+			and(Cin,S1,C2),
+			or(C2,C1,Carry).
