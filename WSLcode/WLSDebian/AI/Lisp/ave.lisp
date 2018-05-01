11c2f51bcce3b49210ceb891058bf8060abe8cd3
AppaBabba <darleg@outlook.com>
2018-05-01 16:49:05 -0700

    clisp code

---
+++ WSLcode/WLSDebian/AI/Lisp/ave.lisp
@@ -0,0 +1,2 @@
+(write-line "This is a function called ave")
+(defun ave (n1 n2) (/ (+ n1 n2) 2))
--- WSLcode/WLSDebian/AI/Lisp/fn.lisp
+++ WSLcode/WLSDebian/AI/Lisp/fn.lisp
@@ -1,1 +1,6 @@
+(write-line "This adds more functions")
 (defun ave (n1 n2) (/ (+ n1 n2) 2))
+(defun add (n1 n2) (+ n1 n2))
+(defun sub (n1 n2) (- n1 n2))
+(defun mul (n1 n2) (* n1 n2))
+(defun div (n1 n2) (/ n1 n2))
