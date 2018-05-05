90840979775f8642cb52b88cefe359a419f7dff0
AppaBabba <darleg@outlook.com>
2018-05-04 17:14:22 -0700

    add more color text lines

--- VScode/C#/CsharpCon/CsharpCon/Program.cs
+++ VScode/C#/CsharpCon/CsharpCon/Program.cs
@@ -10,9 +10,17 @@ namespace CsharpCon
     {
         static void Main(string[] args)
         {
-            Console.BackgroundColor = ConsoleColor.Black;
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Red text over black.");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("************************");
+            Console.WriteLine("*Black text over Green.*");
+            Console.WriteLine("************************");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("************************");
+            Console.WriteLine("*Yellow text over Blue.*");
+            Console.WriteLine("************************");
+
             Console.ReadLine();
         }
     }
